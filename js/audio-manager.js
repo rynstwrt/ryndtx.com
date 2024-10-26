ff25fa86a94415157f77cdc8316afe1db4996515
Ryan Stewart <ryanstewartalex@gmail.com>
2024-10-26 17:55:16 -0500

    Improve Transitions

--- js/audio-manager.js
+++ js/audio-manager.js
@@ -1,136 +1,8 @@
 import { Visualizer } from "./visualizer.js";
 
 
-const TITLE_PREFIX = "Ryn - ";
-const AUDIO_PATH = "assets/audio/";
-const AUDIOS = [
-    {
-        file: "GameBitch.mp3",
-        title: "GameBitch™",
-        year: "2021"
-    },
-    {
-        file: "CeremonialFunk.mp3",
-        title: "Ceremonial Funk",
-        year: "2021"
-    },
-    {
-        file: "ADeeperLove.mp3",
-        title: "A Deeper Love",
-        year: "2022"
-    },
-    {
-        file: "4AM.mp3",
-        title: "4 AM",
-        year: "2022"
-    },
-    {
-        file: "IDontKnowIfIWantToGoYet.mp3",
-        title: "I Don't Know If I Want To Go Yet",
-        year: "2022"
-    },
-    {
-        file: "MaybeIt'llGetBetter.mp3",
-        title: "Maybe It'll Get Better",
-        year: "2022"
-    },
-    {
-        file: "HouseOfPianoHouse.mp3",
-        title: "House of Piano House",
-        year: "2022"
-    },
-    {
-        file: "PianoBeat.mp3",
-        title: "Piano Beat",
-        year: "2022"
-    },
-    {
-        file: "It'llBeFine.mp3",
-        title: "It'll Be Fine",
-        year: "2022"
-    },
-    {
-        file: "It'sAlwaysTheSame.mp3",
-        title: "It's Always the Same",
-        year: "2022"
-    },
-    {
-        file: "I'mGonnaDishYouRightNow.mp3",
-        title: "I'm Gonna Dish You Right Now!",
-        year: "2023"
-    },
-    {
-        file: "ItBeLikeThat.mp3",
-        title: "It Be Like That",
-        year: "2023"
-    },
-    {
-        file: "Gutted.mp3",
-        title: "Gutted",
-        year: "2023"
-    },
-    {
-        file: "Down.mp3",
-        title: "Down",
-        year: "2023"
-    },
-    {
-        file: "Transfiguration.mp3",
-        title: "Transfiguration",
-        year: "2023"
-    },
-    {
-        file: "Growing.mp3",
-        title: "Growing",
-        year: "2023"
-    },
-    {
-        file: "ICantBelieveThisHappened.mp3",
-        title: "I Can't Believe This Happened",
-        year: "2023"
-    },
-    {
-        file: "OnMyMind.mp3",
-        title: "On My Mind",
-        year: "2023"
-    },
-    {
-        file: "Amalgam.mp3",
-        title: "Amalgam",
-        year: "2023"
-    },
-
-    {
-        file: "WhatIfYouFly.mp3",
-        title: "What If You Fly?",
-        year: "2024"
-    },
-    {
-        file: "ItMightBeGettingBetter.mp3",
-        title: "It (Might) Be Getting Better",
-        year: "2024"
-    },
-    {
-        file: "Recovery.mp3",
-        title: "Recovery",
-        year: "2024"
-    },
-    {
-        file: "ImPerfect.mp3",
-        title: "I'm Perfect!",
-        year: "2024"
-    },
-    {
-        file: "MakesUsStronger.mp3",
-        title: "Makes Us Stronger",
-        year: "2024"
-    },
-    {
-        file: "WDYFW.mp3",
-        title: "WDYFW",
-        year: "2024"
-    }
-];
+const TITLE_PREFIX = "RynDTX - ";
+const TRANSITION_TIME_MS = 150;
 
 
 class AudioManager
@@ -219,7 +91,8 @@ class AudioManager
 
     async moveToNextOrPreviousSong(isNext)
     {
-        this.#nowPlaying.textContent = "";
+        this.#nowPlaying.classList.add("hidden");
+
 
         if (this.#shuffling)
         {
@@ -236,14 +109,17 @@ class AudioManager
         }
 
         this.#audio.unload();
-
         const currentAudio = AUDIOS[this.#songIndex];
         await this.#loadSong(AUDIO_PATH + currentAudio.file);
         this.#audio.play();
 
-        this.#nowPlaying.textContent = this.getTitle();
 
-        Visualizer.setSource(this.#audio._sounds[0]._node);
+        setTimeout(async () =>
+        {
+            this.#nowPlaying.textContent = this.getTitle();
+            this.#nowPlaying.classList.remove("hidden");
+            Visualizer.setSource(this.#audio._sounds[0]._node);
+        }, TRANSITION_TIME_MS);
     }
 
 
