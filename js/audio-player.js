5e10d6c5abd9e7bb0128f0e7f8b0752852cd4b6c
Ryan Stewart <ryanstewartalex@gmail.com>
2024-11-02 16:24:50 -0500

    Add EQ And Logo Visualizer

--- js/audio-player.js
+++ js/audio-player.js
@@ -1,12 +1,13 @@
-const songs = [];
+import { Visualizer } from "./visualizer.js";
 
+
+const songs = [];
 for (const audio of AUDIOS)
 {
     const song = {
         name: audio.title,
         artist: "RynDTX  ",
         url: AUDIO_PATH + audio.file,
-        lrc: "lrc1.lrc",
         theme: "#FF6600"
     };
 
@@ -17,15 +18,11 @@ for (const audio of AUDIOS)
 const aPlayer = new APlayer({
     container: document.querySelector("#music-player"),
     listFolded: false,
-    listMaxHeight: 90,
     audio: songs.reverse()
-    // audio: [
-    //     {
-    //         name: "asdf",
-    //         artist: "RynDTX",
-    //         url: AUDIO_PATH + AUDIOS[0].file,
-    //         lrc: "lrc1.lrc",
-    //         theme: "#FF6600"
-    //     }
-    // ]
 });
+
+
+Visualizer.init(aPlayer.audio);
+Visualizer.animate();
+
+
--- js/visualizer.js
+++ js/visualizer.js
@@ -1,6 +1,8 @@
 const BAR_COLOR = "#f84b15";
-const logoContainer = document.querySelector("#logo-container");
 const BAR_WIDTH = 2;
+
+const logoContainer = document.querySelector("#logo-container");
+const canvas = document.querySelector("#eq-canvas");
 
 
 class Visualizer
@@ -10,42 +12,49 @@ class Visualizer
     static canvasContext = undefined;
 
 
-    static init(analyser, canvas)
+    static init(audioElement)
     {
-        Visualizer.analyser = analyser;
-        Visualizer.canvas = canvas;
+        canvas.width = window.innerWidth;
+
+        const audioContext = new AudioContext();
+        Visualizer.analyser = audioContext.createAnalyser();
+
         Visualizer.canvasContext = canvas.getContext("2d");
-    }
 
-
-    static setSource(source)
-    {
-        Visualizer.analyser = Howler.ctx.createAnalyser();
+        const source = audioContext.createMediaElementSource(audioElement);
         source.connect(Visualizer.analyser);
-        Visualizer.analyser.connect(Howler.ctx.destination);
+        Visualizer.analyser.connect(audioContext.destination);
     }
 
 
-    static draw()
+    static animate()
     {
+        window.RequestAnimationFrame = window.requestAnimationFrame(Visualizer.animate)
+            || window.msRequestAnimationFrame(Visualizer.animate)
+            || window.mozRequestAnimationFrame(Visualizer.animate)
+            || window.webkitRequestAnimationFrame(Visualizer.animate);
+
         const fbcArray = new Uint8Array(Visualizer.analyser.frequencyBinCount);
         const barCount = window.innerWidth / 2;
         Visualizer.analyser.getByteFrequencyData(fbcArray);
 
-        Visualizer.canvasContext.clearRect(0, 0, Visualizer.canvas.width, Visualizer.canvas.height);
+        Visualizer.canvasContext.clearRect(0, 0, canvas.width, canvas.height);
         Visualizer.canvasContext.fillStyle = BAR_COLOR;
 
         for (let i = 0; i < barCount; ++i)
         {
             const barPos = i * 4;
             const barHeight = -(fbcArray[i] / 2);
-            Visualizer.canvasContext.fillRect(barPos, Visualizer.canvas.height, BAR_WIDTH, barHeight);
+            Visualizer.canvasContext.fillRect(barPos, canvas.height, BAR_WIDTH, barHeight);
         }
 
         const average = fbcArray.reduce((a, b) => a + b) / fbcArray.length;
         logoContainer.style.transform = `scale(${1 + average / 1000})`;
     }
 }
+
+
+window.addEventListener("resize", () => { canvas.width = window.innerWidth; });
 
 
 export { Visualizer };
