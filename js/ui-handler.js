81a6a2cad65880ce9725f11043ec3c4f406ec0bc
Ryn. <rynstwrt17@gmail.com>
2025-05-29 14:02:54 -0500

    Make header responsive

--- js/ui-handler.js
+++ js/ui-handler.js
@@ -1,21 +1,21 @@
-const CONTACT_EMAIL = "mgmt@ryndtx.com";
-const POPUP_DURATION_MS = 1000;
-
-
-const copiedPopup = document.querySelector("#copied-email-popup");
-
-
-document.querySelector("#email-button").addEventListener("click", () =>
-{
-    if (!copiedPopup.classList.contains("hidden"))
-        return;
-
-    navigator.clipboard.writeText(CONTACT_EMAIL).then(() =>
-    {
-        copiedPopup.classList.remove("hidden");
-        setTimeout(() =>
-        {
-            copiedPopup.classList.add("hidden");
-        }, POPUP_DURATION_MS);
-    });
-});
+// const CONTACT_EMAIL = "mgmt@ryndtx.com";
+// const POPUP_DURATION_MS = 1000;
+//
+//
+// const copiedPopup = document.querySelector("#copied-email-popup");
+//
+//
+// document.querySelector("#email-button").addEventListener("click", () =>
+// {
+//     if (!copiedPopup.classList.contains("hidden"))
+//         return;
+//
+//     navigator.clipboard.writeText(CONTACT_EMAIL).then(() =>
+//     {
+//         copiedPopup.classList.remove("hidden");
+//         setTimeout(() =>
+//         {
+//             copiedPopup.classList.add("hidden");
+//         }, POPUP_DURATION_MS);
+//     });
+// });
